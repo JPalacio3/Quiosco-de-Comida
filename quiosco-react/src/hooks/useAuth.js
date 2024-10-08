909e9c51e9c90c219740e87c9809f7148824a948
unknown <joelpalacio630@gmail.com>
2024-10-08 17:46:52 -0600

    feat - REACT : Redireccionando al usuario a la página de pedidos uan vez que ha sido autenticado

--- quiosco-react/src/hooks/useAuth.js
+++ quiosco-react/src/hooks/useAuth.js
@@ -1,18 +1,51 @@
 /** @format */
+
+import useSWR from "swr";
 import clienteAxios from "../config/axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const useAuth = ({ middleware, url }) => {
+	const token = localStorage.getItem("AUTH_TOKEN");
+	const navigate = useNavigate();
+
+	const {
+		data: user,
+		error,
+		mutate,
+	} = useSWR("/api/user", () =>
+		clienteAxios("/api/user", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((res) => res.data)
+			.catch((error) => {
+				throw Error(error?.response?.data?.errors);
+			}),
+	);
+
 	const login = async (datos, setErrores) => {
 		try {
 			const { data } = await clienteAxios.post("/api/login", datos);
 			localStorage.setItem("AUTH_TOKEN", data.token);
 			setErrores([]);
+			await mutate();
 		} catch (error) {
 			setErrores(Object.values(error.response.data.errors));
 		}
 	};
 	const registro = () => {};
 	const logout = () => {};
+
+	console.log(user);
+	console.log(error);
+
+	useEffect(() => {
+		if (middleware === "guest" && url && user) {
+			navigate("/");
+		}
+	}, [user, error]);
 
 	return {
 		login,
